4ec38135f3b4fa9011c9d6321f34cc225e5f3c67
Henrique Santos <hrqcds.dev@gmail.com>
2025-08-13 18:51:38 -0400

    aaaaaaaa

--- src/lib/lumi.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-    "projectId": "p346406931991007232",
-    "apiBaseUrl": "https://api.lumi.new",
-    "authOrigin": "https://auth.lumi.new",
-})
-    
